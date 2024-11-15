381726685c6e6bc93c95d24635346bb4295befca
Srujana047 <srujanareddymanne07@gmail.com>
2024-11-15 22:37:12 +0530

    Update GYMDAY.py

--- 1200-1400/GYMDAY.py
+++ 1200-1400/GYMDAY.py
@@ -1,1 +1,17 @@
-
+T = int(input())
+for i in range(T):
+    d,x,y = map(int,input().split())
+    c=0
+    n = -1
+    if y>=x:
+        print(0)
+    else:
+        while(y!=0):
+            y-=1
+            c+=1
+            z = (d*c*x)/100
+            b = x-z
+            if(b<=y):
+                n=c
+                break
+        print(n)
