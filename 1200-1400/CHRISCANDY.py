09e300910c7ab84d753147a0bdfa8805e3daac1a
Srujana047 <srujanareddymanne07@gmail.com>
2024-11-15 22:39:04 +0530

    Update CHRISCANDY.py

--- 1200-1400/CHRISCANDY.py
+++ 1200-1400/CHRISCANDY.py
@@ -1,1 +1,12 @@
-
+t = int(input())
+for i in range(t):
+    n = int(input())
+    a = list(map(int,input().split()))
+    b = 0
+    maxi = a[0]
+    for i in range(1,len(a)):
+        if a[i]>maxi:
+            maxi = a[i]
+        else:
+            b+=1
+    print(b)
