f8360906bb1994a6b877fb123ee8fe6931b00639
Srujana047 <srujanareddymanne07@gmail.com>
2024-12-05 20:34:35 +0530

    Create BRKBKS.py

---
+++ 1200-1400/BRKBKS.py
@@ -0,0 +1,9 @@
+T = int(input())
+for i in range(T):
+    s,w1,w2,w3 = map(int,input().split())
+    if w1+w2+w3<=s:
+        print(1)
+    elif w1+w2<=s or w2+w3<=s:
+        print(2)
+    else:
+        print(3)
